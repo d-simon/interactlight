921dba3a583312a1a9db219fb2597272f7aa57fe
David Simon <me@davidsimon.ch>
2014-03-23 12:46:24 +0100

    [Refactor] JSHint

--- interactlight/worldmap.js
+++ interactlight/worldmap.js
@@ -11,7 +11,7 @@ var pixelScreen = require('../screen-36x24.js')
 
 
 
-WorldMap = function () {
+function WorldMap () {
     this.width = pixelScreen.width;
     this.height = pixelScreen.height;
     this.defaultCoordinates = [-180,-80,180,90];
@@ -32,51 +32,51 @@ WorldMap.prototype.start = function (coords, slow, cb) {
     var callback = (typeof cb === 'function') ? cb : false;
     if (coords && coords.length === 4) { this.coordinates = coords; } else { this.coordinates = this.defaultCoordinates; }
     twit.stream('statuses/filter',
-            {
-                filter_level:'none',
-                locations: this.coordinates.join(',')
-            },
-            function (stream) {
-                that.stream = stream;
-                that.running = true;
+        {
+            filter_level:'none',
+            locations: this.coordinates.join(',')
+        },
+        function (stream) {
+            that.stream = stream;
+            that.running = true;
 
-                stream.on('data', function (data) {
-                    //if (data) console.log(data.user.name, data.text.replace(/(\r\n|\n|\r)/gm,""));
-                    var array = [];
-                    if (data.geo) {
-                        var coords = that.normalizeCoordinates(
-                                        [that.coordinates[0]+180, that.coordinates[1]+90],
-                                        [that.coordinates[2]+180, that.coordinates[3]+90],
-                                        [data.geo.coordinates[1]+180, data.geo.coordinates[0]+90]
-                                    );
-                        array.push({
-                            x: ~~(coords[0] * that.width+0.5),
-                            y: ~~(coords[1] * that.height+0.5),
-                            r: 10,
-                            g: 10,
-                            b: 255
-                        });
-                        if (0 <= array[0].y && array[0].y < that.height && 0 <= array[0].x && array[0].x < that.width) {
-                            var led = pixelScreen.image[array[0].y][array[0].x];
-                            if (slowMode === true) {
-                                led[0] = Math.min(led[0] + 1, 255);
-                                led[1] = Math.min(led[1] + 1, 255);
-                                led[2] = Math.min(led[2] + 1, 255);
-                            } else {
-                                led[0] = Math.min(led[0] + 200, 255);
-                                led[1] = Math.min(led[1] + 200, 255);
-                                led[2] = Math.min(led[2] + 200, 255);
-                            }
-                            pixelScreen.forceUpdate();
+            stream.on('data', function (data) {
+                //if (data) console.log(data.user.name, data.text.replace(/(\r\n|\n|\r)/gm,""));
+                var array = [];
+                if (data.geo) {
+                    var coords = that.normalizeCoordinates(
+                                    [that.coordinates[0]+180, that.coordinates[1]+90],
+                                    [that.coordinates[2]+180, that.coordinates[3]+90],
+                                    [data.geo.coordinates[1]+180, data.geo.coordinates[0]+90]
+                                );
+                    array.push({
+                        x: ~~(coords[0] * that.width+0.5),
+                        y: ~~(coords[1] * that.height+0.5),
+                        r: 10,
+                        g: 10,
+                        b: 255
+                    });
+                    if (0 <= array[0].y && array[0].y < that.height && 0 <= array[0].x && array[0].x < that.width) {
+                        var led = pixelScreen.image[array[0].y][array[0].x];
+                        if (slowMode === true) {
+                            led[0] = Math.min(led[0] + 1, 255);
+                            led[1] = Math.min(led[1] + 1, 255);
+                            led[2] = Math.min(led[2] + 1, 255);
                         } else {
-                            console.log('Out of Bounds!', array,  data.geo);
+                            led[0] = Math.min(led[0] + 200, 255);
+                            led[1] = Math.min(led[1] + 200, 255);
+                            led[2] = Math.min(led[2] + 200, 255);
                         }
+                        pixelScreen.forceUpdate();
+                    } else {
+                        console.log('Out of Bounds!', array,  data.geo);
                     }
-                    // TweetCallback
-                    if (cb) cb(data);
-                });
-
+                }
+                // TweetCallback
+                if (cb) cb(data);
             });
+
+        });
 
     if (slowMode !== true) {
         this.reduceInterval = setInterval(function () {
@@ -134,24 +134,24 @@ WorldMap.prototype.twitterCallback = function (data) {
 };
 
 
-WorldMap.prototype.increaseUntil = function (inputVal, max, n) {
-    var n = n || 1;
+WorldMap.prototype.increaseUntil = function (inputVal, max, step) {
+    var n = step || 1;
     return (inputVal < max) ? inputVal + n : max;
-}
-WorldMap.prototype.reduceUntilMin = function (inputVal, min, n) {
-    var min = min || 50;
-    var n = n || 1;
+};
+WorldMap.prototype.reduceUntilMin = function (inputVal, minimum, step) {
+    var min = minimum || 50;
+    var n = step || 1;
     return (inputVal > min) ? inputVal - n : min;
-}
-WorldMap.prototype.reduceUntilZero = function (inputVal, n) {
-    var n = n || 1;
+};
+WorldMap.prototype.reduceUntilZero = function (inputVal, step) {
+    var n = step || 1;
     return (inputVal > n) ? inputVal - n : 0;
-}
-WorldMap.prototype.reduceUntilMinOrZero = function (inputVal, min, n) {
-    var min = min || 50;
-    var n = n || 1;
+};
+WorldMap.prototype.reduceUntilMinOrZero = function (inputVal, minimum, step) {
+    var min = minimums || 50;
+    var n = step || 1;
     if (inputVal === 0) return 0;
     return (inputVal > min) ? Math.max(inputVal - n,min) : min;
-}
+};
 
 module.exports = WorldMap;
