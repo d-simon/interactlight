46028094516dd6ebb127acc3ed18e64591be2efe
David Simon <me@davidsimon.ch>
2014-03-28 15:03:01 +0100

    [Update] Crossbrowser Video // [Feature] WebFont 'Miso' // [Feature] Fixed video-bg changes through waypoint instead of sly 'silly' scroll // [Fix] viewport buggyfill

--- html/js/main.js
+++ html/js/main.js
@@ -1,84 +1,68 @@
-/* global $ */
+/* global $, Modernizr */
 $(function() {
-'use strict';
+    'use strict';
 
-    var options = {
-        scrollBy: 200,
-        speed: 200,
-        easing: 'easeOutQuart',
-        scrollBar: '#scrollbar',
-        activatePageOn: 'click',
-        dynamicHandle: 1,
-        dragHandle: 1,
-        clickBar: 1,
-        mouseDragging: 1,
-        touchDragging: 1,
-        releaseSwing: 1
-    };
-    var frame = new Sly('#frame', options);
+/* ====================================================================
+        Section Resize
+   ==================================================================== */
+
+    window.viewportUnitsBuggyfill.init();
+
+    if (Modernizr.cssvhunit === false || Modernizr.cssvwunit === false) {
+
+        var sectionResize = (function () {
+            var $main = $('*[role="main"]'),
+                $sectionsFull = $main.children('section.js-fullHeight'),
+                $sectionsMin = $main.children('section.js-minHeight');
+
+            return $.debounce(33, function () { // ~30 fps
+                for (var i = 0; i < $sectionsFull.length; i++) {
+                    $($sectionsFull[i]).css('height', $(window).height());
+                }
+                for (var i = 0; i < $sectionsMin.length; i++) {
+                    $($sectionsMin[i]).css('min-height', $(window).height()*1.1);
+                }
+            });
+        })();
+
+        sectionResize();
+
+        $(window).on('resize', sectionResize);
+    }
+
+
+/* ====================================================================
+        VIDEO
+   ==================================================================== */
 
     if (!Modernizr.touch) {
-        // Fix this?
-        setTimeout(function () {
-            // Initiate frame
-            frame.init();
-        }, 200);
-    }
-    // Reload on resize
-    $(window).on('resize', frame.reload);
 
-    var toggleAnimationLoop = (function () {
-        var loopId;
-        var speed = 100;
-
-        return function () {
-            if (loopId) {
-                loopId = clearTimeout(loopId);
-                frame.set(options);
-            } else {
-                var i = 0;
-                frame.set('easing', 'linear');
-                frame.set('speed', speed);
-                (function loop() {
-                    i++;
-                    loopId = setTimeout(loop, speed);
-                    frame[i % 2 === 0 ? 'toStart' : 'toEnd']();
-                } ());
-            }
-        };
-    })();
-
-    var sectionResize = (function () {
-        var $main = $('*[role="main"]'),
-            $sectionsFull = $main.children('section.js-fullHeight'),
-            $sectionsMin = $main.children('section.js-minHeight');
-
-        return $.debounce(16, function () { // 60 fps
-            for (var i = 0; i < $sectionsFull.length; i++) {
-                $($sectionsFull[i]).css('height', $(window).height());
-                console.log($(window).height());
-            }
-            for (var i = 0; i < $sectionsMin.length; i++) {
-                $($sectionsMin[i]).css('min-height', $(window).height()*1.1);
-                console.log($(window).height());
-            }
+        var BV = new $.BigVideo({
+            container:$('#video-bg'),
+            useFlashForFirefox:false
         });
-    })();
 
-    sectionResize();
-
-    $(window).on('resize', sectionResize);
-
-
-    var BV = new $.BigVideo({
-        container:$('#video-bg')
-    });
-
-    if (!Modernizr.touch) {
         BV.init();
-        //BV.show('media/DSC_2710.mp4', { ambient: true });
-        BV.show('media/DSC_2756.mp4', { ambient: true });
-    }
+        BV.show('media/DSC_2710.mp4', { ambient: true, altSource: 'media/DSC_2710.ogv' });
 
+        var changeVideo = (function () {
+            var $videoBg = $('#video-bg').find('video');
+
+            return function (video) {
+                var currentVideo = $videoBg.attr('src');
+                if (currentVideo !== video + '.mp4' && currentVideo !== video + '.ogv') {
+                    BV.show(video + '.mp4', { ambient: true, altSource: video + '.ogv' });
+                }
+            };
+        })();
+
+        $('.home').waypoint(function() {
+            changeVideo('media/DSC_2710');
+        }, { offset: '-100%' });
+
+        $('.how-it-works').waypoint(function () {
+            changeVideo('media/DSC_2756');
+        }, { offset: '100%' });
+    }
 
 });
